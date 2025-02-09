2dd23a848e044eefb5b72c35d2ab2a1b9836315e
Mridul Gupta <mridul1940.be22@chitkara.edu.in>
2025-02-09 13:34:02 +0530

    cors added

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.get('/greet',(req,res)=>{
     res.status(200).json({
         msg : "Hello From Backend"
